1c12ec235dc147bcde413dafe6e2e18853ddf21b
Leocaliban <leocaliban@gmail.com>
2018-09-13 16:02:44 -0300

    Aplica pipes nos campos de datas e de valor

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -58,8 +58,8 @@ export class LancamentosPesquisaComponent implements OnInit {
 export interface Lancamento {
   tipo: string;
   descricao: string;
-  dataVencimento: string;
-  dataPagamento: string;
+  dataVencimento: Date;
+  dataPagamento: Date;
   valor: number;
   pessoa: string;
 }
@@ -68,23 +68,23 @@ const LANCAMENTO_DATA: Lancamento[] = [
   {
     tipo: 'DESPESA',
     descricao: 'Compra de pão',
-    dataVencimento: '30/06/2017',
+    dataVencimento: new Date(2017, 6, 30),
     dataPagamento: null,
     valor: 4.55,
-    pessoa: 'Padaria Pão Novo',
+    pessoa: 'Padaria Pão Novo'
   },
   {
     tipo: 'RECEITA',
     descricao: 'Aluguel',
-    dataVencimento: '11/03/2018',
-    dataPagamento: '11/03/2018',
+    dataVencimento: new Date(2018, 3, 11),
+    dataPagamento: new Date(2018, 3, 11),
     valor: 220.00,
     pessoa: 'Jack Bauer'
   },
   {
     tipo: 'DESPESA',
     descricao: 'Conta de Água',
-    dataVencimento: '11/05/2018',
+    dataVencimento: new Date(2018, 5, 11),
     dataPagamento: null,
     valor: 100.00,
     pessoa: 'James Lancer'
@@ -92,17 +92,18 @@ const LANCAMENTO_DATA: Lancamento[] = [
   {
     tipo: 'RECEITA',
     descricao: 'Salário',
-    dataVencimento: '11/09/2018',
-    dataPagamento: '11/09/2018',
+    dataVencimento: new Date(2018, 9, 11),
+    dataPagamento: new Date(2018, 6, 19),
     valor: 990.00,
     pessoa: 'Rafael Lima'
   },
   {
     tipo: 'DESPESA',
     descricao: 'Cervejas',
-    dataVencimento: '05/04/2018',
-    dataPagamento: '05/04/2018',
-    valor: 90.00,
+    dataVencimento: new Date(2018, 5, 4),
+    dataPagamento: new Date(2018, 5, 4),
+    valor: 90000.00,
     pessoa: 'Zeca'
   }
 ];
+
