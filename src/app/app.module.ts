4e1496f75bb3e4f3d9a2c0f0167d3f070e8c74e8
Leocaliban <leocaliban@gmail.com>
2018-09-30 01:46:04 -0300

    Atualiza Locale da aplicação [pt]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { MatPaginatorIntl } from '@angular/material/paginator';
@@ -12,6 +12,10 @@ import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/core.module';
 import { LancamentoService } from './lancamentos/lancamento.service';
 import { PessoaService } from './pessoas/pessoa.service';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -28,7 +32,8 @@ import { PessoaService } from './pessoas/pessoa.service';
   providers: [
     LancamentoService,
     PessoaService,
-    { provide: MatPaginatorIntl, useValue: getPtPaginator() }
+    { provide: MatPaginatorIntl, useValue: getPtPaginator() },
+    { provide: LOCALE_ID, useValue: 'pt' }
   ],
 
   bootstrap: [AppComponent]
