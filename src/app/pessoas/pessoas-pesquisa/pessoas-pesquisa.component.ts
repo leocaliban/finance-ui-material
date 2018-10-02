c29dfaecaa105c32c10244a7b754c6fd68f9bf42
Leocaliban <leocaliban@gmail.com>
2018-10-02 02:10:34 -0300

    Aplica ConfirmDialog na exclusão de pessoas [sweetalert2]

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { PageEvent, MatTableDataSource, MatPaginator } from '@angular/material';
 import { PessoaFiltro } from '../pessoa.service';
 import { ToastyService } from 'ng2-toasty';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
@@ -46,5 +47,23 @@ export class PessoasPesquisaComponent implements OnInit {
           timeout: 4000
         });
       });
+  }
+
+
+  confirmarExclusao(pessoa: any) {
+    Swal({
+      title: 'Confirmação',
+      text: 'Tem certeza que deseja excluir?',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim, exclua!',
+      cancelButtonText: 'Não'
+    }).then((result) => {
+      if (result.value) {
+        this.excluir(pessoa);
+      }
+    });
   }
 }
