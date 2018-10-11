7fdd186d24fda00152b4fa0e6d6bd063a7128584
Leocaliban <leocaliban@gmail.com>
2018-10-11 01:36:13 -0300

    Implementa o salvamento de pessoas [POST]

--- src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { PessoaService } from '../pessoa.service';
+import { Pessoa } from '../../core/domain/pessoa';
+import { ErrorHandlerService } from '../../core/error-handler.service';
+
+import { ToastyService } from 'ng2-toasty';
 
 @Component({
   selector: 'app-pessoa-cadastro',
@@ -7,9 +14,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoaCadastroComponent implements OnInit {
 
-  constructor() { }
+  pessoa = new Pessoa();
+  constructor(
+    private pessoaService: PessoaService,
+    private errorHandler: ErrorHandlerService,
+    private toastyService: ToastyService) { }
 
   ngOnInit() {
+  }
+
+  salvar(form: FormControl) {
+    this.pessoaService.salvar(this.pessoa)
+      .then(() => {
+        this.toastyService.success({
+          title: 'Sucesso!',
+          msg: 'A pessoa foi salva.',
+          showClose: true,
+          timeout: 4000
+        });
+        form.reset();
+        this.pessoa = new Pessoa();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
 }
